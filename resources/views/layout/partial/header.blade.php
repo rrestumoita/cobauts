4cd392c79d928ae9bb1319a72c29ccf650889b13
rrestumoita <153410233+rrestumoita@users.noreply.github.com>
2024-03-29 13:35:34 +0700

    Update header.blade.php

--- resources/views/layout/partial/header.blade.php
+++ resources/views/layout/partial/header.blade.php
@@ -1,142 +1,142 @@
 <aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 " id="sidenav-main">
 <div class="sidenav-header">
-    <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-    <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/argon-dashboard/pages/dashboard.html " target="_blank">
-    <img src="./assets/img/logo-ct-dark.png" class="navbar-brand-img h-100" alt="main_logo">
-    <span class="ms-1 font-weight-bold">Argon Dashboard 2</span>
-    </a>
+  <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
+  <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/argon-dashboard/pages/dashboard.html " target="_blank">
+  <img src="./assets/img/logo-ct-dark.png" class="navbar-brand-img h-100" alt="main_logo">
+  <span class="ms-1 font-weight-bold">Argon Dashboard 2</span>
+  </a>
 </div>
 <hr class="horizontal dark mt-0">
 <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
-    <ul class="navbar-nav">
-    <li class="nav-item">
-        <a class="nav-link active" href="./pages/dashboard.html">
-        <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-            <i class="ni ni-tv-2 text-primary text-sm opacity-10"></i>
-        </div>
-        <span class="nav-link-text ms-1">Dashboard</span>
-        </a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link " href="./pages/tables.html">
-        <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-            <i class="ni ni-calendar-grid-58 text-warning text-sm opacity-10"></i>
-        </div>
-        <span class="nav-link-text ms-1">Tables</span>
-        </a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link " href="./pages/billing.html">
-        <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-            <i class="ni ni-credit-card text-success text-sm opacity-10"></i>
-        </div>
-        <span class="nav-link-text ms-1">Billing</span>
-        </a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link " href="./pages/virtual-reality.html">
-        <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-            <i class="ni ni-app text-info text-sm opacity-10"></i>
-        </div>
-        <span class="nav-link-text ms-1">Virtual Reality</span>
-        </a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link " href="./pages/rtl.html">
-        <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-            <i class="ni ni-world-2 text-danger text-sm opacity-10"></i>
-        </div>
-        <span class="nav-link-text ms-1">RTL</span>
-        </a>
-    </li>
-    <li class="nav-item mt-3">
-        <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Account pages</h6>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link " href="./pages/profile.html">
-        <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-            <i class="ni ni-single-02 text-dark text-sm opacity-10"></i>
-        </div>
-        <span class="nav-link-text ms-1">Profile</span>
-        </a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link " href="./pages/sign-in.html">
-        <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-            <i class="ni ni-single-copy-04 text-warning text-sm opacity-10"></i>
-        </div>
-        <span class="nav-link-text ms-1">Sign In</span>
-        </a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link " href="./pages/sign-up.html">
-        <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-            <i class="ni ni-collection text-info text-sm opacity-10"></i>
-        </div>
-        <span class="nav-link-text ms-1">Sign Up</span>
-        </a>
-    </li>
-    </ul>
+  <ul class="navbar-nav">
+  <li class="nav-item">
+      <a class="nav-link active" href="./pages/dashboard.html">
+      <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+          <i class="ni ni-tv-2 text-primary text-sm opacity-10"></i>
+      </div>
+      <span class="nav-link-text ms-1">Dashboard</span>
+      </a>
+  </li>
+  <li class="nav-item">
+      <a class="nav-link " href="./pages/tables.html">
+      <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+          <i class="ni ni-calendar-grid-58 text-warning text-sm opacity-10"></i>
+      </div>
+      <span class="nav-link-text ms-1">Tables</span>
+      </a>
+  </li>
+  <li class="nav-item">
+      <a class="nav-link " href="./pages/billing.html">
+      <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+          <i class="ni ni-credit-card text-success text-sm opacity-10"></i>
+      </div>
+      <span class="nav-link-text ms-1">Billing</span>
+      </a>
+  </li>
+  <li class="nav-item">
+      <a class="nav-link " href="./pages/virtual-reality.html">
+      <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+          <i class="ni ni-app text-info text-sm opacity-10"></i>
+      </div>
+      <span class="nav-link-text ms-1">Virtual Reality</span>
+      </a>
+  </li>
+  <li class="nav-item">
+      <a class="nav-link " href="./pages/rtl.html">
+      <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+          <i class="ni ni-world-2 text-danger text-sm opacity-10"></i>
+      </div>
+      <span class="nav-link-text ms-1">RTL</span>
+      </a>
+  </li>
+  <li class="nav-item mt-3">
+      <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Account pages</h6>
+  </li>
+  <li class="nav-item">
+      <a class="nav-link " href="./pages/profile.html">
+      <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+          <i class="ni ni-single-02 text-dark text-sm opacity-10"></i>
+      </div>
+      <span class="nav-link-text ms-1">Profile</span>
+      </a>
+  </li>
+  <li class="nav-item">
+      <a class="nav-link " href="./pages/sign-in.html">
+      <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+          <i class="ni ni-single-copy-04 text-warning text-sm opacity-10"></i>
+      </div>
+      <span class="nav-link-text ms-1">Sign In</span>
+      </a>
+  </li>
+  <li class="nav-item">
+      <a class="nav-link " href="./pages/sign-up.html">
+      <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+          <i class="ni ni-collection text-info text-sm opacity-10"></i>
+      </div>
+      <span class="nav-link-text ms-1">Sign Up</span>
+      </a>
+  </li>
+  </ul>
 </div>
 <div class="sidenav-footer mx-3 ">
-    <div class="card card-plain shadow-none" id="sidenavCard">
-    <img class="w-50 mx-auto" src="./assets/img/illustrations/icon-documentation.svg" alt="sidebar_illustration">
-    <div class="card-body text-center p-3 w-100 pt-0">
-        <div class="docs-info">
-        <h6 class="mb-0">Need help?</h6>
-        <p class="text-xs font-weight-bold mb-0">Please check our docs</p>
-        </div>
-    </div>
-    </div>
-    <a href="https://www.creative-tim.com/learning-lab/bootstrap/license/argon-dashboard" target="_blank" class="btn btn-dark btn-sm w-100 mb-3">Documentation</a>
-    <a class="btn btn-primary btn-sm mb-0 w-100" href="https://www.creative-tim.com/product/argon-dashboard-pro?ref=sidebarfree" type="button">Upgrade to pro</a>
+  <div class="card card-plain shadow-none" id="sidenavCard">
+  <img class="w-50 mx-auto" src="./assets/img/illustrations/icon-documentation.svg" alt="sidebar_illustration">
+  <div class="card-body text-center p-3 w-100 pt-0">
+      <div class="docs-info">
+      <h6 class="mb-0">Need help?</h6>
+      <p class="text-xs font-weight-bold mb-0">Please check our docs</p>
+      </div>
+  </div>
+  </div>
+  <a href="https://www.creative-tim.com/learning-lab/bootstrap/license/argon-dashboard" target="_blank" class="btn btn-dark btn-sm w-100 mb-3">Documentation</a>
+  <a class="btn btn-primary btn-sm mb-0 w-100" href="https://www.creative-tim.com/product/argon-dashboard-pro?ref=sidebarfree" type="button">Upgrade to pro</a>
 </div>
 </aside>
 </main>
-  <div class="fixed-plugin">
-    <a class="fixed-plugin-button text-dark position-fixed px-3 py-2">
-      <i class="fa fa-cog py-2"> </i>
-    </a>
-    <div class="card shadow-lg">
-      <div class="card-header pb-0 pt-3 ">
-        <div class="float-start">
-          <h5 class="mt-3 mb-0">Argon Configurator</h5>
-          <p>See our dashboard options.</p>
-        </div>
-        <div class="float-end mt-4">
-          <button class="btn btn-link text-dark p-0 fixed-plugin-close-button">
-            <i class="fa fa-close"></i>
-          </button>
-        </div>
-        <!-- End Toggle Button -->
+<div class="fixed-plugin">
+  <a class="fixed-plugin-button text-dark position-fixed px-3 py-2">
+    <i class="fa fa-cog py-2"> </i>
+  </a>
+  <div class="card shadow-lg">
+    <div class="card-header pb-0 pt-3 ">
+      <div class="float-start">
+        <h5 class="mt-3 mb-0">Argon Configurator</h5>
+        <p>See our dashboard options.</p>
       </div>
-      <hr class="horizontal dark my-1">
-      <div class="card-body pt-sm-3 pt-0 overflow-auto">
-        <!-- Sidebar Backgrounds -->
-        <div>
-          <h6 class="mb-0">Sidebar Colors</h6>
+      <div class="float-end mt-4">
+        <button class="btn btn-link text-dark p-0 fixed-plugin-close-button">
+          <i class="fa fa-close"></i>
+        </button>
+      </div>
+      <!-- End Toggle Button -->
+    </div>
+    <hr class="horizontal dark my-1">
+    <div class="card-body pt-sm-3 pt-0 overflow-auto">
+      <!-- Sidebar Backgrounds -->
+      <div>
+        <h6 class="mb-0">Sidebar Colors</h6>
+      </div>
+      <a href="javascript:void(0)" class="switch-trigger background-color">
+        <div class="badge-colors my-2 text-start">
+          <span class="badge filter bg-gradient-primary active" data-color="primary" onclick="sidebarColor(this)"></span>
+          <span class="badge filter bg-gradient-dark" data-color="dark" onclick="sidebarColor(this)"></span>
+          <span class="badge filter bg-gradient-info" data-color="info" onclick="sidebarColor(this)"></span>
+          <span class="badge filter bg-gradient-success" data-color="success" onclick="sidebarColor(this)"></span>
+          <span class="badge filter bg-gradient-warning" data-color="warning" onclick="sidebarColor(this)"></span>
+          <span class="badge filter bg-gradient-danger" data-color="danger" onclick="sidebarColor(this)"></span>
         </div>
-        <a href="javascript:void(0)" class="switch-trigger background-color">
-          <div class="badge-colors my-2 text-start">
-            <span class="badge filter bg-gradient-primary active" data-color="primary" onclick="sidebarColor(this)"></span>
-            <span class="badge filter bg-gradient-dark" data-color="dark" onclick="sidebarColor(this)"></span>
-            <span class="badge filter bg-gradient-info" data-color="info" onclick="sidebarColor(this)"></span>
-            <span class="badge filter bg-gradient-success" data-color="success" onclick="sidebarColor(this)"></span>
-            <span class="badge filter bg-gradient-warning" data-color="warning" onclick="sidebarColor(this)"></span>
-            <span class="badge filter bg-gradient-danger" data-color="danger" onclick="sidebarColor(this)"></span>
-          </div>
-        </a>
-        <!-- Sidenav Type -->
-        <div class="mt-3">
-          <h6 class="mb-0">Sidenav Type</h6>
-          <p class="text-sm">Choose between 2 different sidenav types.</p>
-        </div>
-        <div class="d-flex">
-          <button class="btn bg-gradient-primary w-100 px-3 mb-2 active me-2" data-class="bg-white" onclick="sidebarType(this)">White</button>
-          <button class="btn bg-gradient-primary w-100 px-3 mb-2" data-class="bg-default" onclick="sidebarType(this)">Dark</button>
-        </div>
-        <p class="text-sm d-xl-none d-block mt-2">You can change the sidenav type just on desktop view.</p>
+      </a>
+      <!-- Sidenav Type -->
+      <div class="mt-3">
+        <h6 class="mb-0">Sidenav Type</h6>
+        <p class="text-sm">Choose between 2 different sidenav types.</p>
+      </div>
+      <div class="d-flex">
+        <button class="btn bg-gradient-primary w-100 px-3 mb-2 active me-2" data-class="bg-white" onclick="sidebarType(this)">White</button>
+        <button class="btn bg-gradient-primary w-100 px-3 mb-2" data-class="bg-default" onclick="sidebarType(this)">Dark</button>
+      </div>
+      <p class="text-sm d-xl-none d-block mt-2">You can change the sidenav type just on desktop view.</p>
 
-      </div>
     </div>
   </div>
+</div>
